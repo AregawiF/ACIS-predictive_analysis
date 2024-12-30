d45a18ba3ef698f36b8a2e26cc54742099851a26
AregawiF <133565354+AregawiF@users.noreply.github.com>
2024-12-30 22:23:43 +0300

    completed eda on insurance data

---
+++ scripts/__init__.py
@@ -0,0 +1,11 @@
+
+# This is the __init__.py file for the ACIS-predictive_analysis scripts package.
+# It can be used to initialize the package and import necessary modules.
+
+# Example of importing a module from the package
+
+# You can also define package-level variables or functions here
+__version__ = "1.0.0"
+
+def package_function():
+    print("This is a function in the ACIS-predictive_analysis scripts package.")
